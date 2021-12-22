79538699af907caf1c5604f605eca47e37b032e7
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-12-22 00:39:30 -0600

    wip: adds update controller/route

--- controllers/pantry.js
+++ controllers/pantry.js
@@ -14,7 +14,7 @@ Router.get('/pantry', async (req, res) => {
 
 // Create Route 
 Router.post('/pantry', async (req, res) => {
-    try{
+    try {
         res.json(await Pantry.create(req.body));
     } catch (error) {
         res.status(400).json(error);
@@ -24,11 +24,26 @@ Router.post('/pantry', async (req, res) => {
 
 // Delete Route
 Router.delete('/pantry/:id', async (req, res) => {
-    try{
+    try { 
         res.json(await Pantry.findByIdAndDelete(req.params.id));
     } catch (error) {
         res.status(400).json(error);
     }
 });
+
+// Update Route
+Router.put('/pantry/:id', async (req, res) => {
+    try {
+        res.json(await Pantry.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new:true })
+        );
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+
 module.exports = Router
         
