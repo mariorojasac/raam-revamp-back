d2052a9f74ad4238ab2ccd72bdd1c32b8d5241f4
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-12-22 18:54:40 -0600

    wip: adds food model

--- models/pantry.js
+++ models/pantry.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const pantrySchema = new Schema({
     img: { type: String },
+    name: { type: String},
     location: { type: String},
     description: { type: String }
 });
